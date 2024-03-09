e2f4979a39495875afcbdb14753939d8e84a8a6c
Scarlett <haojiakong626@gmail.com>
2024-03-09 18:09:07 +0100

    summary

--- synod/src/main/java/com/example/synod/Process.java
+++ synod/src/main/java/com/example/synod/Process.java
@@ -5,19 +5,25 @@ import akka.actor.Props;
 import akka.actor.UntypedAbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
-import com.example.synod.message.Launch;
+
+import com.example.synod.Main.LaunchMsg;
+import com.example.synod.Main.CrashMsg;
+//import com.example.synod.message.Launch;
 import com.example.synod.message.Membership;
 
 import java.util.Random;
 
 public class Process extends UntypedAbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);// Logger attached to actor
+    private boolean faultProneMode = false;
+    private boolean silentMode = false;
 
     private int n;//number of processes
     private int i;//id of current process
     private Membership processes;//other processes' references
-    private Boolean proposal;
+    private Integer proposal;
     private int ballot;
+    private final double alpha = 0.3;
 
     /*
      * Static method to create an actor
@@ -32,20 +38,37 @@ public class Process extends UntypedAbstractActor {
         this.ballot = i-n;
     }
 
-    private void propose(Boolean v) {
+    private void propose(Integer v) {
         log.info(this + " - propose("+ v+")");
-        proposal = v;
-        ballot += n;
+        this.proposal = v;
+        this.ballot += n;
     }
 
     public void onReceive(Object message) throws Throwable {
+        //Silent Mode
+        if(this.silentMode){
+            return;
+        }else if(this.faultProneMode){//Fault Prone Mode
+            boolean crash = (((new Random()).nextFloat())<this.alpha);
+            if(crash){
+                this.silentMode = true;
+                return;
+            }
+        }
+
+
         if (message instanceof Membership) {
             log.info(this + " - membership received");
             Membership m = (Membership) message;
             processes = m;
-        } else if (message instanceof Launch) {
+        } else if (message instanceof LaunchMsg) {
             log.info(this + " - launch received");
-            propose(true);
+            int v = (new Random()).nextInt(2);
+            this.propose(v);
+
+        } else if (message instanceof CrashMsg){
+            this.log.info("p" + self().path().name()+" received LAUNCH.");
+            this.faultProneMode = true;
         }
     }
     
